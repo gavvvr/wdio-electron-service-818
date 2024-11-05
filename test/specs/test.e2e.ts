38c8e739548a2b74ff6da3d64dd9223835d64d77
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2024-11-06 00:12:20 +0300

    Fix test by adding `await`

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,11 +1,11 @@
 describe("My Login application", () => {
   it("should reset the frame when the page is reloaded", async () => {
     await expect($("#tinymce")).not.toBePresent();
-    await browser.switchFrame($("iframe"));
+    await browser.switchFrame(await $("iframe"));
     await expect($("#tinymce")).toBePresent();
     await browser.refresh();
     await expect($("#tinymce")).not.toBePresent();
-    await browser.switchFrame($("iframe"));
+    await browser.switchFrame(await $("iframe"));
     await expect($("#tinymce")).toBePresent();
   });
 });
